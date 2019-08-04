d1d588bf2cba4b2c0b6c14b6bf828a02270b9cd7
Tsai Mujing <moojitsai@gmail.com>
2019-08-04 20:05:51 +0800

    init index.js page

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,11 @@ import Router from 'next/router';
 function IndexPage() {
   return (
     <div>
-      <Head>
-        <title>My page title</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <p>Home Page</p>
+      <h1>Home page</h1>
     </div>
   );
 }
 IndexPage.getInitialProps= ({res})=>{
-  if (res) {
-    res.writeHead(302, {
-      Location: '/member'
-    })
-    res.end()
-  } else {
-    Router.push('/member')
-  }
+ 
 } 
 export default IndexPage;
